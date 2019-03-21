25afbfbbceea72e5d4c85b6ec515c7d9a059b79d
dondo1998 <48430222+dondo1998@users.noreply.github.com>
2019-03-21 20:45:45 +0700

    Add files via upload

---
+++ konfigurasi_database.php
@@ -0,0 +1,8 @@
+<?php 
+$host = "localhost";
+$user = "id8249471_restoran1";
+$pass = "12345678";
+$db = "id8249471_restoran";
+ 
+$conn = mysqli_connect($host,$user,$pass,$db) or die("Error" . mysqli_error($conn));
+?>
